9095f0b1b4377f228cae1fc1fe7fb8825869925a
houstonlws <houstonhvrd@gmail.com>
2021-04-21 23:16:43 -0400

    tests kind of working

--- src/test/java/com/link/notifications/dao/NotificationDaoTest.java
+++ src/test/java/com/link/notifications/dao/NotificationDaoTest.java
@@ -1,50 +1,38 @@
 package com.link.notifications.dao;
 
-import com.link.notifications.config.NotificationsJpaConfig;
+import com.link.notifications.NotificationsApplication;
 import com.link.notifications.model.Notification;
-import com.link.notifications.service.NotificationService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(
-        classes = { NotificationsJpaConfig.class },
-        loader = AnnotationConfigContextLoader.class)
-@Transactional
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes= NotificationsApplication.class)
+@DataJpaTest
 class NotificationDaoTest {
 
-    @Resource
+    @Autowired
     private NotificationDao dao;
 
-    @Autowired
-    public NotificationDaoTest(NotificationDao dao) {
-        this.dao = dao;
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE}) // six numbers
-    void findByTargetId(Notification n) {
-        n = new Notification();
-        n.setTriggeredId(1);
-        n.setTargetId(2);
-        n.setRead(false);
-        n.setType("like");
+    @Test
+    void findByTargetId() {
+        Notification n = new Notification();
+            n.setTriggeredId(1);
+            n.setTargetId(2);
+            n.setRead(false);
+            n.setType("like");
 
         dao.save(n);
-        dao.findByTargetId(2);
+        List<Notification> dbNote = dao.findByTargetId(2);
+
+        Assert.assertNotNull(dbNote);
     }
 
     @Test
