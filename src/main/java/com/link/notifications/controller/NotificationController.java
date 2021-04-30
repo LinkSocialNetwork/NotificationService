69a70f7f4fd39027dee6a75fd7519420cc6b79de
houstonlws <houstonhvrd@gmail.com>
2021-04-30 16:30:09 -0400

    Changed database to h2

--- src/main/java/com/link/notifications/controller/NotificationController.java
+++ src/main/java/com/link/notifications/controller/NotificationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/api/notificationservice")
 public class NotificationController {
 
     private NotificationService notificationServ;
