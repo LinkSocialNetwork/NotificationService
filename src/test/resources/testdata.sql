fa60557210896219d7de73eea80d348603bff28c
houstonlws <houstonhvrd@gmail.com>
2021-05-05 16:15:22 -0400

    tests worked on slightly

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,9 +1,11 @@
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 1, default, 1, 2, 'like');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 2, default, 1, 3, 'comment');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 4, default, 1, 6, 'post');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 1, default, 2, 6, 'post');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 3, default, 2, 5, 'comment');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 8, default, 4, 3, 'like');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 9, default, 6, 3, 'comment');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 7, default, 5, 1, 'like');
-insert into notifications (id, date, post_id, read, target_id, triggered_id, type) values (default, current_timestamp, 2, default, 7, 1, 'post');
+insert into notifications
+(id, date, post_id, read, target_id, triggered_id, type)
+values (default, '0000-00-00 00:00:00', 1, default, 1, 2, 'liked');
+
+insert into notifications
+(id, date, post_id, read, target_id, triggered_id, type)
+values (default, '0000-00-00 00:00:00', 2, default, 2, 3, 'commented');
+
+insert into notifications
+(id, date, post_id, read, target_id, triggered_id, type)
+values (default, current_timestamp, 4, default, 3, 6, 'liked');
