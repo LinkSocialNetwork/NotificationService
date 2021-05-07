38472f44887690b83211270e56a611ae98eff1e0
chakeem95 <chakeem95@gmail.com>
2021-05-07 09:35:28 -0700

    notification service

--- src/test/java/com/link/notifications/service/NotificationServiceTest.java
+++ src/test/java/com/link/notifications/service/NotificationServiceTest.java
@@ -1,15 +1,29 @@
 package com.link.notifications.service;
 
+import com.link.notifications.dao.NotificationDao;
+import com.link.notifications.model.Notification;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class NotificationServiceTest {
+
+    @Mock
+    private NotificationDao notificationDao;
+
+    private NotificationService notificationService;
 
     @BeforeEach
     void setUp() {
+        notificationService = new NotificationService(notificationDao);
     }
 
     @AfterEach
@@ -18,6 +32,20 @@ class NotificationServiceTest {
 
     @Test
     void getAll() {
+        Date myDate;
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH, 8);
+        cal.set(Calendar.DATE, 24);
+        cal.set(Calendar.YEAR, 2000);
+        myDate=cal.getTime();
+        Notification expected = new Notification(1,2,3,4, true, myDate);
+
+        Mockito.when(notificationDao.findById(1)).thenReturn(expected);
+
+        Notification actual = notificationService.getOne(1);
+
+        assertEquals(expected,actual);
+
     }
 
     @Test
@@ -46,6 +74,9 @@ class NotificationServiceTest {
 
     @Test
     void updateOne() {
+        Notification temp = new Notification();
+        notificationService.updateOne(temp);
+        Mockito.verify(notificationDao).save(temp);
     }
 
     @Test
