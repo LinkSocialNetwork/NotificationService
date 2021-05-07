3bfec9f611902fff04083125331aa9f8f2010766
Rafael Malespin <rafael.malespin@revature.net>
2021-05-07 12:03:46 -0400

    Added Notification Dao Test Cases

--- src/test/resources/testdata.sql
+++ src/test/resources/testdata.sql
@@ -1,11 +1,11 @@
 insert into notifications
 (id, date, post_id, read, target_id, triggered_id, type)
-values (default, current_timestamp, 1, default, 1, 2, 'liked');
+values (default, {ts '1969-12-31 19:00:00.00'}, 1, default, 1, 2, 'liked');
 
 insert into notifications
 (id, date, post_id, read, target_id, triggered_id, type)
-values (default, current_timestamp, 2, default, 2, 3, 'commented');
+values (default,  {ts '1969-12-31 19:00:00.00'}, 2, default, 2, 3, 'commented');
 
 insert into notifications
 (id, date, post_id, read, target_id, triggered_id, type)
-values (default, current_timestamp, 4, default, 3, 6, 'liked');
+values (default, {ts '1969-12-31 19:00:00.00'}, 4, default, 3, 6, 'liked');
