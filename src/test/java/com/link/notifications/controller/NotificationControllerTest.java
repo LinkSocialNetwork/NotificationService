7e67330596123a1de374b42a16849f2a3a81d07a
chakeem95 <chakeem95@gmail.com>
2021-05-07 08:09:28 -0700

    notification test controller setup

--- src/test/java/com/link/notifications/controller/NotificationControllerTest.java
+++ src/test/java/com/link/notifications/controller/NotificationControllerTest.java
@@ -1,27 +1,42 @@
 package com.link.notifications.controller;
 
 import com.link.notifications.model.Notification;
+import com.link.notifications.service.NotificationService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class NotificationControllerTest {
 
     @Autowired
     NotificationController notificationController;
 
+    @Mock
+    NotificationService notificationService;
+
+    @BeforeEach
+    void setUp() {notificationController = new NotificationController(notificationService);}
+
     //----------------------------------------------------------------------------------------------//
 
     @Test
     void getAll() {
-        List<Notification> list = notificationController.getAll(1);
-        System.out.println(list);
+        Notification not1 = new Notification();
+        Notification not2 = new Notification();
+        Notification not3 = new Notification();
+        List<Notification> testList = new ArrayList<>();
+
     }
 
     //----------------------------------------------------------------------------------------------//
